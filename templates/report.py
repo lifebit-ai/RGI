e3d84eb40c2e18a5292bc14d3f2e07d08bb92daf
imendes93 <ines@lifebit.ai>
2021-01-07 16:07:45 +0000

    report refactor with plotly

--- templates/report.py
+++ templates/report.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import ast
 import pandas as pd
 import plotly.graph_objs as go
 from plotly.offline import plot
@@ -9,79 +10,94 @@ SUMMARY_TABLE = "$summary_df"
 HITS_TABLE = "$hit_table"
 HEATMAP = "$heatmap_df"
 
-def create_table_tracer(header_values, header_font, header_line, header_fill,
-                        cells_values, cells_font, cells_line, cells_fill,
-                        domain):
-    """
-    """
+colorscale = ['#f0f0f0', '#5a6e92','#04a0fc']
 
-    tracer = go.Table(header=dict(values=header_values,
-                                  font=header_font,
-                                  align='left',
-                                  line=header_line,
-                                  fill=header_fill),
-                      cells=dict(values=cells_values,
-                                 font=cells_font,
-                                 align='left',
-                                 line=cells_line,
-                                 fill=cells_fill),
-                      domain=domain)
+conversion = {0: "No hit", 1: "Strict", 2: "Perfect"}
 
-    return tracer
-
+def convert_tuple(tup):
+    try:
+        tup = ast.literal_eval(tup)
+        str =  ', '.join(tup) 
+        return str
+    except:
+        return tup
 
 def df_to_plotly(df):
     return {'z': df.values.tolist(),
             'x': df.columns.tolist(),
             'y': df.index.tolist()}
 
-
+def get_hovertext(df):
+    hovertext = list()
+    for yi, yy in enumerate(df.index):
+        hovertext.append(list())
+        for xi, xx in enumerate(df.columns.tolist()):
+            hovertext[-1].append('Sample: {}<br />Symbol: {}<br />Hit Type: {}'.format(xx, yy, conversion[df.iat[yi, xi]]))
+    return hovertext
+    
 def main(summary_file, hits_table, heatmap):
-    
-    # main report 
-    fig = make_subplots(rows=3, cols=2, vertical_spacing=0.1, 
-                        specs=[[{"type": "table"}, None],
-                              [{"colspan": 2}, None],
-                              [{"colspan": 2, "type": "table"}, None]],
-                        subplot_titles=("Summary Table", "Hits Heatmap", "Hits Table"))
 
-    # summary table
+    # load data files
+    #   - summary: number of hits per sample 
     summary_df = pd.read_csv(summary_file)
-    fig.add_trace(go.Table(header=dict(values=["Sample", "Perfect", "Strict", "Loose"],
-                           font=dict(size=10), align="left"),
-                           cells=dict(values=[summary_df[k].tolist() for k in summary_df.columns[0:]], 
-                                      align = "left")),
-                  row=1, col=1)
-    
-    # heatmap 
-    conversion = {0: "No hit", 1: "Strict", 2: "Perfect"}
-
+    #   - heatmap: pivot table data
     df_heatmat = pd.read_csv(heatmap)
     df_heatmat = df_heatmat.set_index(df_heatmat.columns[0])
-    colorscale = ['#ffffff', '#5a6e92','#04a0fc']
-    hovertext = list()
-    for yi, yy in enumerate(df_heatmat.index):
-        hovertext.append(list())
-        for xi, xx in enumerate(df_heatmat.columns.tolist()):
-            hovertext[-1].append('Sample: {}<br />Symbol: {}<br />Hit Type: {}'.format(xx, yy, conversion[df_heatmat.iat[yi, xi]]))
-    
-    fig.add_trace(go.Heatmap(df_to_plotly(df_heatmat), colorscale=colorscale, 
-    hoverinfo='text', text=hovertext, showscale=False), row=2, col=1)
+    #   - hits table: comprehensive results table
+    hits_df = pd.read_csv(hits_table)
+    hits_df = hits_df.applymap(convert_tuple)
 
-    fig.update_layout(title={'text': "RGI-nf Report",
-                             'y':0.95,
-                             'x':0.5,
-                             'xanchor': 'center',
-                             'yanchor': 'top',
-                             'font': {'size': 24}})
+    # main report skeleton 
+    fig = make_subplots(rows=8, cols=2, vertical_spacing=0.1, 
+                    specs=[[{"type": "table", "rowspan": 2, "colspan": 2}, None],
+                           [None, None],
+                           [{"type": "table", "rowspan": 2, "colspan": 2}, None],
+                           [None, None],
+                           [{"type": "xy", "rowspan":2, "colspan": 2}, None],
+                           [None, None],
+                           [{"type": "table", "rowspan":2 ,"colspan": 2}, None],
+                           [None, None]],
+                        subplot_titles=("","Summary Table", "Hits Heatmap", "Hits Table"))
+
+    # intro text
+    summary = ('RGI-nf integrates <a href="https://github.com/arpcard/rgi">The Resistance Gene Identifier (RGI)</a> package<br>'
+               'to predict resistome(s) from nucleotide data based on homology and SNP models.<br> '
+               ' <br>'
+               'The application uses reference data from the <a href="https://card.mcmaster.ca/">Comprehensive Antibiotic Resistance Database (CARD)</a>.'
+               ' <br>'
+               'A total of {0} samples were analysed, detecting a total of {1} unique AMR genes.'
+               ' <br>'.format(len(summary_df.columns[0]), len(hits_df.index)))
+
+    fig.add_annotation(x=0, xref='paper', y=1, yref='paper', text=summary,
+                       showarrow=False, font=dict(size=14), align='center', 
+                       bgcolor='#f0f0f0')
+
+    # summary table
+    fig.add_trace(go.Table(header=dict(values=["Sample", "Perfect", "Strict", "Loose"],
+                           font=dict(size=14), align="center", fill_color="#04a0fc"),
+                           cells=dict(values=[summary_df[k].tolist() for k in summary_df.columns[0:]], 
+                                      align = ["left", "center"], fill_color='#f0f0f0')),
+                  row=3, col=1)
+    
+    # heatmap 
+    hovertext = get_hovertext(df_heatmat)
+    fig.add_trace(go.Heatmap(df_to_plotly(df_heatmat), colorscale=colorscale, 
+                             hoverinfo='text', text=hovertext, showscale=False), row=5, col=1)
     
     # hits table
-    hits_df = pd.read_csv(hits_table)
     fig.add_trace(go.Table(header=dict(values=["Gene Symbol", "Gene Family", "Drug Class", "Resistance Mechanism"],
-                        font=dict(size=10), align="left"),
+                        font=dict(size=14), align="center", fill_color="#04a0fc"),
                         cells=dict(values=[hits_df[k].tolist() for k in hits_df.columns[0:]], 
-                                    align = "left")),
-                row=3, col=1)
+                                    align = "left",  fill_color='#f0f0f0')),
+                row=7, col=1)
+    
+    # title
+    fig.update_layout(title={'text': "<b>RGI-nf Report</b>",
+                            'y':0.95,
+                            'x':0.5,
+                            'xanchor': 'center',
+                            'yanchor': 'top',
+                            'font': {'size': 24, 'color': '#04a0fc'}})
     fig.show()
     
 
